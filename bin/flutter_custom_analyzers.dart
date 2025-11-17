180c923ffa00c4e8ba5c9425f81c9d356440ee90
Loïc Sharma <sharma.loic@gmail.com>
2025-11-16 16:32:14 -0800

    Start

---
+++ bin/flutter_custom_analyzers.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_custom_analyzers/flutter_custom_analyzers.dart' as flutter_custom_analyzers;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_custom_analyzers.calculate()}!');
+}
---
+++ test/flutter_custom_analyzers_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_custom_analyzers/flutter_custom_analyzers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
